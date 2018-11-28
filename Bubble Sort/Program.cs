bb39f8fe12bf3464b328c4c830dc3faf48e454f5
Zhi Zhao (CSI Interfusion Inc) <v-zhazhi@microsoft.com>
2018-11-28 14:35:36 +0800

    Bubble_Sort

--- Bubble Sort/Program.cs
+++ Bubble Sort/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Bubble_Sort
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int[] array = { 43, 69, 11, 72, 56, 24, 5, 3, 7 };
+            BubbleSort(array);
+            ShowSord(array);
+
+            Console.ReadKey();
+        }
+
+        static void ShowSord(int[] array)
+        {
+            foreach (var num in array)
+            {
+                Console.WriteLine($"{num}");
+            }
+            Console.WriteLine();
+        }
+
+        static void BubbleSort(int[] array)
+        {
+            int swap;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        swap = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = swap;
+                    }
+                }
+            }
+
         }
     }
 }
