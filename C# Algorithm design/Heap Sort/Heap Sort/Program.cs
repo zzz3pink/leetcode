8749dba4d5be1afe24c9ab6e0b10e25658efc6ed
Zhi Zhao (CSI Interfusion Inc) <v-zhazhi@microsoft.com>
2019-05-08 17:05:43 +0800

    update

---
+++ C# Algorithm design/Heap Sort/Heap Sort/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//a.将无序序列构建成一个堆,大顶堆
+//b.将堆顶元素和末尾元素交换，将最大元素沉到数组末端
+//c.重新调整结构，使其满足堆定义，然后继续交换堆顶元素和当前末尾元素。递归
+namespace Heap_Sort
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            int[] array = { 10, 24, 5, 3, 7, 12 };
+            HeapSort(array);
+            ShowSort(array);
+
+            Console.ReadKey();
+        }
+
+        private static void ShowSort(int[] array) 
+        {
+            foreach (var num in array)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+
+        private static void HeapSort(int[] array)
+        {
+            MaxHeap(array);
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                Swap(ref array[0], ref array[i]);
+                Heapify(array, 0, i);
+            }
+        }
+
+        private static void MaxHeap(int[] array)
+        {
+            for (int i = array.Length / 2 - 1; i >= 0; i--)
+            {
+                Heapify(array, i, array.Length);
+            }
+        }
+
+        private static void Heapify(int[] array, int index, int size)
+        {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int large = index;
+
+            if (left < size && array[left] > array[large])
+            {
+                large = left;
+            }
+            if (right < size && array[right] > array[large])
+            {
+                large = right;
+            }
+
+            if(index!=large)
+            {
+                Swap(ref array[index], ref array[large]);
+                Heapify(array, large, size);
+            }
+        }
+
+        private static void Swap(ref int first, ref int second)
+        {
+            int t = first;
+            first = second;
+            second = t;
+        }
+    }
+}
---
+++ C# Algorithm design/Merge Sort/Merge Sort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//分治算法  Divide and Conquer 
+namespace Merge_Sort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = { 43, 69, 11, 72, 56, 24, 5, 3, 7 };
+
+            MergeSort(array, 0, array.Length - 1);
+            ShowSort(array);
+
+            Console.ReadKey();
+        }
+
+        private static void ShowSort(int[] array)
+        {
+            foreach(var num in array)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void MergeSort(int[] array, int low, int high)
+        {
+
+        }
+    }
+}
